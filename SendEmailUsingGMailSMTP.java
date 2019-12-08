ef0b74ad2ceefaf7fa1e4c03619fb8f5a775dd3e
Manta-Kumari-Laksam <47364923+Manta-Kumari-Laksam@users.noreply.github.com>
2019-12-08 12:56:24 +0530

    Add files via upload

---
+++ SendEmailUsingGMailSMTP.java
@@ -0,0 +1,74 @@
+package com.tutorialspoint;
+
+import java.net.SocketException;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmailUsingGMailSMTP{
+	 public static void main(String[] args) {
+	      // Recipient's email ID needs to be mentioned.
+	      String to = "manta.laksam@orange.com";//change accordingly
+
+	      // Sender's email ID needs to be mentioned
+	      String from = "olsmanta@gmail.com";//change accordingly
+	      final String username = "olsmanta";//change accordingly
+	      final String password = "Orange@123";//change accordingly
+
+	     
+	      String host = "dhcp3.equant.com";
+	      
+//	      String host = "smtp.gmail.com";
+	      //String host = "127.0.0.1";
+
+	      Properties props = new Properties();
+	      props.put("mail.smtp.auth", "true");
+	      props.put("mail.smtp.starttls.enable", "true");
+	      props.put("mail.smtp.host", host);
+	      props.put("mail.smtp.port", "587");
+	      
+//	      props.put("mail.smtp.socketFactory.port", "465");
+//	      props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+	      // Get the Session object.
+	      Session session = Session.getInstance(props,
+	      new javax.mail.Authenticator() {
+	         protected PasswordAuthentication getPasswordAuthentication() {
+	            return new PasswordAuthentication(username, password);
+	         }
+	      });
+	      session.setDebug(true);
+
+	      try {
+	         // Create a default MimeMessage object.
+	         Message message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+
+	         // Set To: header field of the header.
+	         message.setRecipients(Message.RecipientType.TO,
+	         InternetAddress.parse(to));
+
+	         // Set Subject: header field
+	         message.setSubject("Testing Subject");
+
+	         // Now set the actual message
+	         message.setText("Hello, this is sample for to check send "
+	            + "email using JavaMailAPI ");
+
+	         // Send message
+	         Transport.send(message);
+
+	         System.out.println("Sent message successfully....");
+
+	      } catch (MessagingException e) {
+	            throw new RuntimeException(e);
+	      }
+	   }
+}
